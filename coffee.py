d53e78da8af849700c66bcf40c576a7fb3e3ab6d
Jacob <mwaniki.jacob@gmail.com>
2018-11-17 19:49:06 +0300

    added different options for difrent prefrences

--- coffee.py
+++ coffee.py
@@ -1,16 +1,28 @@
 #coffee machine, pogramming logic practice
-def make_coffee():
+def make_coffee(*options):
     """makes coffee"""
     ingredients= ["coffee", "Hot_Water"]
+    if options:
+
+        ingredients.append(','.join(options))
     print ("take a mug")
     print("Tear the coffee sachet ")
     print ("pour into the  {}".format (','.join(ingredients)))
     print ("stir your coffee for atleast 5 seconds")
-    my_coffee = "Tasty coffee"
+
+    if options:
+        my_coffee= "Tasty coffee with {}".format(','.join(options))
+    else:
+        my_coffee = "Tasty coffee"
     return my_coffee
 def serves_coffee(make_coffee, Person_served):
     """serves coffee"""
     print ("hello, have your {} {}".format(make_coffee,Person_served))
 
-me= make_coffee()
-serves_coffee(me, 'Jafi')
+plain=make_coffee()
+withS=make_coffee('sugar')
+withM=make_coffee('milk')
+withMaS= make_coffee('milk','and sugar')
+serves_coffee(plain, 'Jafi')
+serves_coffee(withS, 'Jemo')
+serves_coffee(withMaS, 'Jafi')
