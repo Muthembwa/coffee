303f3e4c8c11a9a4c52d646bf60c2ee103e2aa24
Jacob <mwaniki.jacob@gmail.com>
2018-11-17 19:17:17 +0300

    add stir step in the procedure

--- coffee.py
+++ coffee.py
@@ -5,6 +5,7 @@ def make_coffee():
     print ("take a mug")
     print("Tear the coffee sachet ")
     print ("pour into the  {}".format (','.join(ingredients)))
+    print ("stir your coffee for atleast 5 seconds")
     my_coffee = "Tasty coffee"
     return my_coffee
 def serves_coffee(make_coffee, Person_served):
