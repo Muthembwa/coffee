697a47e247f7c9ae90a0f95afc860de2a2a5aa68
Jacob <mwaniki.jacob@gmail.com>
2018-11-17 18:57:57 +0300

    my initial file makes and serves coffee

---
+++ coffee.py
@@ -0,0 +1,15 @@
+#coffee machine, pogramming logic practice
+def make_coffee():
+    """makes coffee"""
+    ingredients= ["coffee", "Hot_Water"]
+    print ("take a mug")
+    print("Tear the coffee sachet ")
+    print ("pour into the  {}".format (','.join(ingredients)))
+    my_coffee = "Tasty coffee"
+    return my_coffee
+def serves_coffee(make_coffee, Person_served):
+    """serves coffee"""
+    print ("hello, have your {} {}".format(make_coffee,Person_served))
+
+me= make_coffee()
+serves_coffee(me, 'Jafi')
